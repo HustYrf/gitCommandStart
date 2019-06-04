d7e1b935dca2ecedb5c966ae36f7790f9ec88600
杨瑞丰 <aishan2@douyu.tv>
2019-06-04 13:38:57 +0800

    refactor(base64Test):test merge to master

--- base64Test.go
+++ base64Test.go
@@ -6,6 +6,9 @@ import (
 )
 
 func main() {
+	var str1 = "杨瑞丰斗鱼"
 	str, _ := base64.StdEncoding.DecodeString("MEE3OEQyQUJBMTk4NEE1MzgzOEQ4Q0JENEYyN0VDNTJ8NS45MDA")
 	fmt.Println(string(str))
+	str2 := fmt.Sprintf("str1`s length:%d", len([]rune(str1)))
+	println(str2)
 }
