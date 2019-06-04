480c0e98cc7c5fe3e7b77fbe5aad2c2b2409f052
杨瑞丰 <aishan2@douyu.tv>
2019-06-04 11:48:51 +0800

    feat(init):add readme.txt

--- base64Test.go
+++ base64Test.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 )
 
-func main(){
-	str,_:=base64.StdEncoding.DecodeString("MEE3OEQyQUJBMTk4NEE1MzgzOEQ4Q0JENEYyN0VDNTJ8NS45MDA")
+func main() {
+	str, _ := base64.StdEncoding.DecodeString("MEE3OEQyQUJBMTk4NEE1MzgzOEQ4Q0JENEYyN0VDNTJ8NS45MDA")
 	fmt.Println(string(str))
 }
